01abbce0245e56e91ca2dab8b8def7cc546d941e
Aigars Šusters <asusters@Aigarss-MBP.lan>
2019-08-30 14:03:37 +0300

    Create initial directory structure

---
+++ spec/protect_spec.rb
@@ -0,0 +1,14 @@
+RSpec.describe "Protect" do
+    describe "Login" do
+      it "go to login screen" do
+        puts "go"
+      end
+      it "enter username and password" do
+        puts "username and password"
+      end
+      it "login is successfull" do
+        puts "success"
+      end
+    end
+  end
+  
